6ba389bcf4c053e1021bed3eb2d95a41def173a8
Ryan Kelley <ryan@ninecollective.com>
2014-03-28 11:18:18 -0500

    Putting pieces together

---
+++ cluster.js
@@ -0,0 +1,20 @@
+var recluster = require('recluster'),
+    path = require('path');
+
+if(process.argv.length < 3) {
+    console.log('You must specify script name');
+    process.exit(1);
+}
+
+var scriptName = process.argv[2];
+
+
+var cluster = recluster(path.join(__dirname, scriptName));
+cluster.run();
+
+process.on('SIGUSR2', function() {
+    console.log('Got SIGUSR2, reloading cluster...');
+    cluster.reload();
+});
+
+console.log("spawned cluster, kill -s SIGUSR2", process.pid, "to reload");
